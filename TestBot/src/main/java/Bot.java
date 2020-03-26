af9ec53f0a0c40be624a9938cdba66d5c0b69c17
pandadrakon <kpw1988@gmail.com>
2020-03-26 09:37:18 +0200

    start work

---
+++ TestBot/src/main/java/Bot.java
@@ -0,0 +1,29 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public String getBotUsername() {
+        return "MyTestWebBot_bot";
+    }
+
+    public String getBotToken() {
+        return "1103663635:AAFtnS46dfpDORy8OiGi0YbdRASwvxdWOrU";
+    }
+}
