7102b1a9cdd129acc3a142d7e35babcad673e2ba
pandadrakon <kpw1988@gmail.com>
2020-03-26 20:53:47 +0200

    work test run miracle answer

---
+++ TestBot/src/main/java/Main.java
@@ -0,0 +1,19 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        try {
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
